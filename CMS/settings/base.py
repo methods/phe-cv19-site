75d445306baa32f48b0776afacfdd5d9a824f41c
whos4n3 <58296855+whos4n3@users.noreply.github.com>
2020-03-23 22:58:21 +0000

    changed DB connection values to pick up the details from Beanstalk env variables

--- CMS/settings/base.py
+++ CMS/settings/base.py
@@ -108,16 +108,22 @@ WSGI_APPLICATION = 'CMS.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.1/ref/settings/#databases
-
-if 'RDS_HOSTNAME' in os.environ:
+if LOCAL:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        }
+    }
+else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql',
-            'NAME': os.environ['RDS_DB_NAME'],
-            'USER': os.environ['RDS_USERNAME'],
-            'PASSWORD': os.environ['RDS_PASSWORD'],
-            'HOST': os.environ['RDS_HOSTNAME'],
-            'PORT': os.environ['RDS_PORT'],
+            'NAME': os.environ.get('DBNAME'),
+            'HOST': os.environ.get('DBHOST'),
+            'USER': os.environ.get('DBUSER'),
+            'PORT': os.environ.get('DBPORT'),
+            'PASSWORD': os.environ.get('DBPASSWORD')
         }
     }
 
