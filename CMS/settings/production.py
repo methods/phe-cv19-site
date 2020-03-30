1946db252e1b281cda69b0b254c3c6ce2f03bd60
Vivian Allen <vivian.allen@methods.co.uk>
2020-03-30 13:02:49 +0100

    typo

--- CMS/settings/production.py
+++ CMS/settings/production.py
@@ -52,7 +52,7 @@ BAKERY_VIEWS = (
 
 # File upload settings
 
-DEFAULT_FILE_STORAGE = 'storages.b1ackends.s3boto3.S3Boto3Storage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
