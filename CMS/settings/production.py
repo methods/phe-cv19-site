347697e48121b41e6daa499aeab001ac89684e79
mwnicks <matthew.nicks@methods.co.uk>
2020-04-01 14:51:14 +0100

    switch cache settings to cloudfront

--- CMS/settings/production.py
+++ CMS/settings/production.py
@@ -36,11 +36,9 @@ LOGGING = {
 }
 
 WAGTAILFRONTENDCACHE = {
-    'cloudflare': {
-        'BACKEND': 'wagtail.contrib.frontend_cache.backends.CloudflareBackend',
-        'EMAIL': os.environ.get('CLOUDFLARE_EMAIL'),
-        'TOKEN': os.environ.get('CLOUDFLARE_TOKEN'),
-        'ZONEID': os.environ.get('CLOUDFLARE_ZONEID'),
+    'cloudfront': {
+        'BACKEND': 'wagtail.contrib.frontend_cache.backends.CloudfrontBackend',
+        'DISTRIBUTION_ID': os.environ.get('AWS_DISTRIBUTION_ID'),
     },
 }
 
