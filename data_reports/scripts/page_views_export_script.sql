da5b39a1ea89bc4ebe54bdaf65be6a99d151400c
mwnicks <matthew.nicks@methods.co.uk>
2020-04-29 16:58:16 +0100

    update sql statements to use new table names

--- data_reports/scripts/page_views_export_script.sql
+++ data_reports/scripts/page_views_export_script.sql
@@ -1,16 +1,16 @@
-SELECT date_format(date_add('day', -1, current_date), '%e/%b/%Y') AS date, "path", COUNT("cloudfront_logs"."page_views"."path") AS requests, 'Success' AS response_type
-FROM "cloudfront_logs"."page_views"
-WHERE "cloudfront_logs"."page_views"."datetime" LIKE concat('[', date_format(date_add('day', -1, current_date), '%e/%b/%Y'), '%')
-        AND "cloudfront_logs"."page_views"."method"='"GET'
-        AND ("cloudfront_logs"."page_views"."response_code" LIKE '2%'
-        OR "cloudfront_logs"."page_views"."response_code" LIKE '3%')
-GROUP BY  "cloudfront_logs"."page_views"."path"
+SELECT date_format(date_add('day', -1, current_date), '%e/%b/%Y') AS date, "path", COUNT("cloudfront_logs"."new_page_views"."path") AS requests, 'Success' AS response_type
+FROM "cloudfront_logs"."new_page_views"
+WHERE "cloudfront_logs"."new_page_views"."datetime" LIKE concat('[', date_format(date_add('day', -1, current_date), '%e/%b/%Y'), '%')
+        AND "cloudfront_logs"."new_page_views"."method"='"GET'
+        AND ("cloudfront_logs"."new_page_views"."response_code" LIKE '2%'
+        OR "cloudfront_logs"."new_page_views"."response_code" LIKE '3%')
+GROUP BY  "cloudfront_logs"."new_page_views"."path"
 UNION
-SELECT date_format(date_add('day', -1, current_date), '%e/%b/%Y') AS date, "path", COUNT("cloudfront_logs"."page_views"."path") AS requests, 'Error' AS response_type
-FROM "cloudfront_logs"."page_views"
-WHERE "cloudfront_logs"."page_views"."datetime" LIKE concat('[', date_format(date_add('day', -1, current_date), '%e/%b/%Y'), '%')
-        AND "cloudfront_logs"."page_views"."method"='"GET'
-        AND ("cloudfront_logs"."page_views"."response_code" LIKE '4%'
-        OR "cloudfront_logs"."page_views"."response_code" LIKE '5%')
-GROUP BY  "cloudfront_logs"."page_views"."path"
+SELECT date_format(date_add('day', -1, current_date), '%e/%b/%Y') AS date, "path", COUNT("cloudfront_logs"."new_page_views"."path") AS requests, 'Error' AS response_type
+FROM "cloudfront_logs"."new_page_views"
+WHERE "cloudfront_logs"."new_page_views"."datetime" LIKE concat('[', date_format(date_add('day', -1, current_date), '%e/%b/%Y'), '%')
+        AND "cloudfront_logs"."new_page_views"."method"='"GET'
+        AND ("cloudfront_logs"."new_page_views"."response_code" LIKE '4%'
+        OR "cloudfront_logs"."new_page_views"."response_code" LIKE '5%')
+GROUP BY  "cloudfront_logs"."new_page_views"."path"
 ORDER BY  "path"
