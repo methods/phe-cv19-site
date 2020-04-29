4313e8b2721061b98c133703221899ae1f788077
mwnicks <matthew.nicks@methods.co.uk>
2020-04-29 17:22:55 +0100

    update sql to both use same field

--- data_reports/scripts/downloads_export_script.sql
+++ data_reports/scripts/downloads_export_script.sql
@@ -1,18 +1,18 @@
-SELECT date_format(date_add('day', -1, current_date), '%e/%b/%Y') AS date, "object", COUNT("cloudfront_logs"."new_resource_downloads"."object") AS requests, 'Success' AS response_type
+SELECT date_format(date_add('day', -1, current_date), '%e/%b/%Y') AS date, "path", COUNT("cloudfront_logs"."new_resource_downloads"."path") AS requests, 'Success' AS response_type
 FROM "cloudfront_logs"."new_resource_downloads"
 WHERE "cloudfront_logs"."new_resource_downloads"."datetime" LIKE concat('[', date_format(date_add('day', -1, current_date), '%e/%b/%Y'), '%')
-        AND "cloudfront_logs"."new_resource_downloads"."object" LIKE 'documents/%'
+        AND "cloudfront_logs"."new_resource_downloads"."path" LIKE 'documents/%'
         AND "cloudfront_logs"."new_resource_downloads"."method"='"GET'
         AND ("cloudfront_logs"."new_resource_downloads"."response_code" LIKE '2%'
         OR "cloudfront_logs"."new_resource_downloads"."response_code" LIKE '3%')
-GROUP BY  "cloudfront_logs"."new_resource_downloads"."object"
+GROUP BY  "cloudfront_logs"."new_resource_downloads"."path"
 UNION
-SELECT date_format(date_add('day', -1, current_date), '%e/%b/%Y') AS date, "object", COUNT("cloudfront_logs"."new_resource_downloads"."object") AS requests, 'Error' AS response_type
+SELECT date_format(date_add('day', -1, current_date), '%e/%b/%Y') AS date, "path", COUNT("cloudfront_logs"."new_resource_downloads"."path") AS requests, 'Error' AS response_type
 FROM "cloudfront_logs"."new_resource_downloads"
 WHERE "cloudfront_logs"."new_resource_downloads"."datetime" LIKE concat('[', date_format(date_add('day', -1, current_date), '%e/%b/%Y'), '%')
-        AND "cloudfront_logs"."new_resource_downloads"."object" LIKE 'documents/%'
+        AND "cloudfront_logs"."new_resource_downloads"."path" LIKE 'documents/%'
         AND "cloudfront_logs"."new_resource_downloads"."method"='"GET'
         AND ("cloudfront_logs"."new_resource_downloads"."response_code" LIKE '4%'
         OR "cloudfront_logs"."new_resource_downloads"."response_code" LIKE '5%')
-GROUP BY  "cloudfront_logs"."new_resource_downloads"."object"
-ORDER BY  "object"
+GROUP BY  "cloudfront_logs"."new_resource_downloads"."path"
+ORDER BY  "path"
