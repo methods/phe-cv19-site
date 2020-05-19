b37785b213e9a9d73d98cbb3052dbff4be0ffe24
mwnicks <matthew.nicks@methods.co.uk>
2020-05-19 10:34:17 +0100

    define basic redirect override class

---
+++ core/models/config.py
@@ -0,0 +1,9 @@
+from wagtail.contrib.redirects.models import Redirect
+
+
+class MethodsRedirect(Redirect):
+  def save(self, *args, **kwargs):
+    super().save(*args, **kwargs)
+
+  def __str__(self):
+    return "{0} -> {1}".format(self.old_path, self.link)
