65b5cb543d5b71497fb310b261ba840eb594ecca
Matt Nicks <matthew.nicks@methods.co.uk>
2020-11-16 15:25:07 +0000

    refactor the reordering assets to separate files

---
+++ CMS/static/js/reorder.js
@@ -0,0 +1,45 @@
+(function($) {
+  var ResourceSort = function(sortToggle, resourcesArea) {
+    this.sortToggle = sortToggle;
+    this.resourcesArea = resourcesArea;
+    this.setup();
+  }
+
+  ResourceSort.prototype = {
+    setup: function() {
+      this.sortToggle.parent().show();
+      this.newestResourceList = this.resourcesArea.find('#newest');
+      this.oldestResourceList = this.resourcesArea.find('#oldest');
+      this.startWatcher();
+    },
+
+    startWatcher: function() {
+      var that = this;
+      this.sortToggle.change(function () {
+        that.handleReorder();
+      })
+    },
+
+    handleReorder: function() {
+      if (this.sortToggle[0].value === 'newest') {
+        this.oldestResourceList.hide();
+        this.newestResourceList.show();
+      } else if (this.sortToggle[0].value === 'oldest') {
+        this.newestResourceList.hide();
+        this.oldestResourceList.show();
+      }
+    }
+  }
+
+  function init() {
+    var sortSelector = $('#order-selector__toggle');
+    var sortItems = $('#internal-resources');
+    if (sortSelector.length > 0 && sortItems.length > 0) {
+      new ResourceSort(sortSelector, sortItems);
+    }
+  }
+
+  $(document).on('page:load', init);
+  $(init);
+
+}(jQuery));
